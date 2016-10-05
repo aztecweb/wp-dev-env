b92f89459e837b5eab33c6f05b25c4e5dc24ef05
Eduardo Pittol <edpittol@gmail.com>
2016-10-05 11:54:40 -0300

    Execute extra package installtion on composer update process

--- src/Script/UpdateScript.php
+++ src/Script/UpdateScript.php
@@ -3,7 +3,10 @@
 namespace MyEnvPress\Script;
 
 use Composer\Script\Event;
-use MyEnvPress\Helper\Command;
+use MyEnvPress\Helper\CommandHelper;
+use MyEnvPress\Extra\Themes;
+use MyEnvPress\Extra\Plugins;
+use MyEnvPress\Extra\Data;
 
 class UpdateScript
 {
@@ -18,5 +21,20 @@ class UpdateScript
 		
 		echo "Updating WordPress...\n";
 		$command->run('core update');
+		
+		echo "Configuring site...\n";
+		// remove wp-config file to override it
+		exec('rm public/wp-config.php');
+		$command->run('core config');
+
+		echo "Installing extra packages and data...\n";
+		$themes = new Themes();
+		$themes->execute();
+		
+		$plugins = new Plugins();
+		$plugins->execute();
+		
+		$data = new Data();
+		$data->execute();
 	}
 }
