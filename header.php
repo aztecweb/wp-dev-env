484b50f25efcb0ad4f597e6e5b368185e637147a
Eduardo Pittol <edpittol@gmail.com>
2016-12-29 00:08:00 -0200

    Remove trash files

--- header.php
+++
@@ -1,6 +0,0 @@
-<!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
-<head>
-<?php wp_head(); ?>
-</head>
-<body>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php get_header() ?>
-    <?php while( have_posts() ) : the_post(); ?>
-        <h1><?php the_title() ?></h1>
-        <?php the_content() ?>
-    <?php endwhile; ?>
-<?php get_footer() ?>
